11e4f2615cb51ab330ae6504ed3975cc159dae1b
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-17 05:25:09 +0530

    create controllers

--- ITC/routes/web.php
+++ ITC/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',function(){
+    echo "Chamathka";
+});
+
+/*Route::get('/about', function () {
+    return view('aboutus');
+});*/
+
+Route::get('/about','PagesController@indexaboutus');
