5a5f73265e9f2b7b33ae13144ae9d37f7862588d
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-17 13:47:42 +0530

    Data Insertion

--- TaskApp/routes/web.php
+++ TaskApp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/tasks', function () {
     return view('tasks');
 });
+
+Route::post('/saveTask', [TaskController::class, 'store']);
