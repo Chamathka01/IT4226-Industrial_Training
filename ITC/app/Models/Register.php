3b8c54d168ed36cf961dad9a9e45e48251e17211
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-18 12:24:14 +0530

    Craete factory data

--- ITC/app/Models/Register.php
+++ ITC/app/Models/Register.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Register extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'registers';
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'dob',
+        'phone',
+        'email',
+        'username',
+        'role',
+        'address',
+        'password',
+    ];
 }
