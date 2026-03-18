8e083033c33ee645a12a99056842a2bb90ced1e0
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-19 04:02:24 +0530

    Add RegisterController

--- ITC/app/Http/Controllers/RegisterController.php
+++ ITC/app/Http/Controllers/RegisterController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Register;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|unique:registers',
+            'username' => 'required|unique:registers',
+            'password' => 'required|min:6',
+        ]);
+
+        Register::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'dob' => $request->dob,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'username' => $request->username,
+            'role' => $request->role,
+            'address' => $request->address,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('/login');
+    }
 }
