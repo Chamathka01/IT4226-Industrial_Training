10dc6a7cc2e1ae2685859c7975c551d89d8e789a
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-18 05:42:58 +0530

    Update Navigation

--- TaskApp/resources/views/welcome.blade.php
+++ TaskApp/resources/views/welcome.blade.php
@@ -4,15 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TaskApp</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .title {
+            font-size: 100px;
+            font-weight: 700;
+             color: black;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="Container d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
+        <div class="title mb-4">
+        Daily Task App
+    </div>
+
+    <a href="/tasks" class="btn btn-primary btn-lg">
+        View Tasks
+    </a>
+
+</div>
+    
+ </body>
+</html>
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TaskApp</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class = "container text-center"> 
         <h1 class="font-weight-bold display-3">Daily Task App</h1>    
     </div>
+
 </body>
 </html>
-
+-->
 <!--
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
