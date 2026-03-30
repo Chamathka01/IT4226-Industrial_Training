d8b0593fe87ac0bd5cb33d0536cd6d173b7224f0
Chamathka01 <chamathkahettiarachchi@gmail.com>
2026-03-30 10:06:25 +0530

    Add reset password

--- ITC/resources/views/reset-password.blade.php
+++ ITC/resources/views/reset-password.blade.php
@@ -30,5 +30,21 @@
     </form>
 </div>
 
+<script>
+function togglePassword(fieldId, icon) {
+    let input = document.getElementById(fieldId);
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
+</script>
+
 </body>
 </html>
